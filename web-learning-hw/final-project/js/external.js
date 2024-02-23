1241b655db3c3ec3393f7a42fbe9da259d7fd083
Ermegilius <147325520+Ermegilius@users.noreply.github.com>
2024-02-23 18:21:02 +0300

    Add files via upload

---
+++ web-learning-hw/final-project/js/external.js
@@ -0,0 +1,9 @@
+
+  function openNav() {
+    document.getElementById("mySidenav").style.display = "inline-block";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+  }
+
